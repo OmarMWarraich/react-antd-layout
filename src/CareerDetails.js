b7b179dce73395d022bd0cdc5f718cc1d8eb76ce
OmarMWarraich <earthling986@gmail.com>
2021-11-02 12:04:01 -0400

    Update App.js, and Add src/CareerDetails and Cricketer.js

---
+++ src/CareerDetails.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {  Drawer } from 'antd';
+
+const CareerDetails = ({ player, visible, onClose }) => {
+    return(
+        <Drawer
+            destroyOnClose
+            title={player}
+            visible={visible}
+            width={640}
+            onClose={onClose}
+            >
+
+            </Drawer>
+    )
+}
+
+export default CareerDetails;
