895b516094862a284dcbf1ccf8f28d857903875a
OmarMWarraich <earthling986@gmail.com>
2021-11-02 10:45:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Icon from '@ant-design/icons';
+import { Layout, Avatar, Menu, Breadcrumb } from 'antd';
+import Title from 'antd/lib/typography/Title';
+import SubMenu from 'antd/lib/menu/SubMenu';
+
+
+const { Header, Footer, Sider, Content } = Layout;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header style={{padding:10}}>
+          <Avatar style={{float:'right'}} src='./dp.png'/>
+          <Title style={{color:'white'}} level={3}>OMAR</Title>
+        </Header>
+        <Layout>
+          <Sider>
+            <Menu
+            defaultSelectedKeys={['Dashboard']}
+            mode="inline"
+            >
+              <Menu.Item key="Dashboard">
+                Dashboard
+              </Menu.Item>
+              <SubMenu
+              title={
+                <span>
+                  <Icon type="mail" />
+                  <span>About Us</span>
+                  </span>
+              }
+              >
+                <Menu.ItemGroup key="About US" title='Country 1'>
+                  <Menu.Item key='location1'>Location 1</Menu.Item>
+                  <Menu.Item key='location2'>Location 2</Menu.Item>
+                </Menu.ItemGroup>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Content style={{ padding: '0 50px'}}>
+              <Breadcrumb style={{ margin: '16px 0'}}>
+              </Breadcrumb>
+              <div style={{ background: '#fff', padding: 24, minHeight: 580}}>Content</div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design Layout example</Footer>
+            </Layout>
+        </Layout>
+      </Layout>
     </div>
   );
 }
